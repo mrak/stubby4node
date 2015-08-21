e12f57e3aacd21664f6e4657f04024a9d6a36db6
Eric Mrak <mail@ericmrak.info>
2015-08-21 16:15:57 -0700

    Move prettyprint tests to js

---
+++ spec/prettyprint.spec.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var sut = require('../lib/console/prettyprint');
+var assert = require('assert');
+
+describe('prettyprint', function () {
+  describe('spacing', function () {
+    it('should return an empty string if given no parameters', function () {
+      var actual = sut.spacing()
+
+        assert(actual === '');
+    });
+
+    it('should return five spaces if given 5', function () {
+      var actual = sut.spacing(5);
+
+      assert(actual === '     ');
+    });
+
+    it('should return empty string if given negative number', function () {
+      var actual = sut.spacing(-5);
+
+      assert(actual === '');
+    });
+  });
+
+  describe('wrap', function () {
+    it('should linebreak at word instead of character given tokens', function () {
+      var continuationIndent = 0;
+      var columns = 25;
+      var words = "one fish, two fish, red fish, blue fish".split(' ');
+      var actual = sut.wrap(words, continuationIndent, columns);
+
+      assert(actual === 'one fish, two fish, red\nfish, blue fish');
+    });
+
+    it('should indent before subsequent lines', function () {
+      var continuationIndent = 5;
+      var columns = 25;
+      var words = "one fish, two fish, red fish, blue fish".split(' ');
+      var actual = sut.wrap(words, continuationIndent, columns);
+
+      assert(actual === 'one fish, two fish,\n     red fish, blue fish');
+    });
+
+    it('should wrap past multiple lines', function () {
+      var continuationIndent = 5;
+      var columns = 15;
+      var words = "one fish, two fish, red fish, blue fish".split(' ');
+      var actual = sut.wrap(words, continuationIndent, columns);
+
+      assert(actual === 'one fish,\n     two fish,\n     red fish,\n     blue fish');
+    });
+  });
+});
