fa486cb8bef92d5a0ec4327a63f1275e90fb2506
Eric Mrak <mail@ericmrak.info>
2015-08-21 16:54:17 -0700

    lint test files, too

--- test/helpers/waits-for.js
+++ test/helpers/waits-for.js
@@ -3,28 +3,30 @@
 var assert = require('assert');
 
 module.exports = function waitsFor(fn, message, range, finish, time) {
-  if (time == null) { time = process.hrtime(); }
-
+  var temp, seconds, nanoseconds, elapsed;
   var min = range[0] != null ? range[0] : 0;
   var max = range[1] != null ? range[1] : range;
-  var temp = time == null ? process.hrtime() : process.hrtime(time);
-  var seconds = temp[0];
-  var nanoseconds = temp[1];
-  var elapsed = seconds * 1000 + nanoseconds / 1000000;
 
-  assert(elapsed < max, "Timed out waiting " + max + "ms for " + message);
+  if (time == null) { time = process.hrtime(); }
+
+  temp = time == null ? process.hrtime() : process.hrtime(time);
+  seconds = temp[0];
+  nanoseconds = temp[1];
+  elapsed = seconds * 1000 + nanoseconds / 1000000;
+
+  assert(elapsed < max, 'Timed out waiting ' + max + 'ms for ' + message);
 
   if (fn()) {
-    assert(elapsed > min, "Condition succeeded before " + min + "ms were up");
+    assert(elapsed > min, 'Condition succeeded before ' + min + 'ms were up');
     return finish();
   }
 
   if (setImmediate != null) {
-    setImmediate(function() {
+    setImmediate(function () {
       waitsFor(fn, message, range, finish, time);
     });
   } else {
-    setTimeout(function() {
+    setTimeout(function () {
       waitsFor(fn, message, range, finish, time);
     }, 5);
   }
--- test/prettyprint.js
+++ test/prettyprint.js
@@ -6,9 +6,9 @@ var assert = require('assert');
 describe('prettyprint', function () {
   describe('spacing', function () {
     it('should return an empty string if given no parameters', function () {
-      var actual = sut.spacing()
+      var actual = sut.spacing();
 
-        assert(actual === '');
+      assert(actual === '');
     });
 
     it('should return five spaces if given 5', function () {
@@ -28,7 +28,7 @@ describe('prettyprint', function () {
     it('should linebreak at word instead of character given tokens', function () {
       var continuationIndent = 0;
       var columns = 25;
-      var words = "one fish, two fish, red fish, blue fish".split(' ');
+      var words = 'one fish, two fish, red fish, blue fish'.split(' ');
       var actual = sut.wrap(words, continuationIndent, columns);
 
       assert(actual === 'one fish, two fish, red\nfish, blue fish');
@@ -37,7 +37,7 @@ describe('prettyprint', function () {
     it('should indent before subsequent lines', function () {
       var continuationIndent = 5;
       var columns = 25;
-      var words = "one fish, two fish, red fish, blue fish".split(' ');
+      var words = 'one fish, two fish, red fish, blue fish'.split(' ');
       var actual = sut.wrap(words, continuationIndent, columns);
 
       assert(actual === 'one fish, two fish,\n     red fish, blue fish');
@@ -46,7 +46,7 @@ describe('prettyprint', function () {
     it('should wrap past multiple lines', function () {
       var continuationIndent = 5;
       var columns = 15;
-      var words = "one fish, two fish, red fish, blue fish".split(' ');
+      var words = 'one fish, two fish, red fish, blue fish'.split(' ');
       var actual = sut.wrap(words, continuationIndent, columns);
 
       assert(actual === 'one fish,\n     two fish,\n     red fish,\n     blue fish');
