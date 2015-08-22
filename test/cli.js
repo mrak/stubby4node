936d06fb90c92222e70fc4eb0ec6f18310d9e818
Eric Mrak <mail@ericmrak.info>
2015-08-21 19:15:00 -0700

    formatting cli tests

--- test/cli.js
+++ test/cli.js
@@ -4,131 +4,156 @@ var assert = require('assert');
 
 describe('CLI', function () {
   var out, sut;
-  sut = null;
-  out = null;
+
   beforeEach(function () {
     sut = require('../src/console/cli');
     out = require('../src/console/out');
     this.sandbox.stub(process, 'exit');
-    return this.sandbox.stub(out, 'log');
+    this.sandbox.stub(out, 'log');
   });
-  afterEach(function () {
-    process.exit.restore();
-    return out.log.restore();
-  });
+
   describe('version', function () {
-    return it('should return the version of stubby in package.json', function () {
-      var expected;
-      expected = require('../package.json').version;
+    it('should return the version of stubby in package.json', function () {
+      var expected = require('../package.json').version;
+
       sut.version(true);
-      return assert(out.log.calledWith(expected));
+
+      assert(out.log.calledWith(expected));
     });
   });
+
   describe('help', function () {
-    return it('should return help text', function () {
+    it('should return help text', function () {
       sut.help(true);
-      return assert(out.log.calledOnce);
+
+      assert(out.log.calledOnce);
     });
   });
+
   describe('getArgs', function () {
     describe('-a, --admin', function () {
       it('should return default if no flag provided', function () {
-        var actual, expected;
-        expected = 8889;
-        actual = sut.getArgs([]);
-        return assert(actual.admin === expected);
+        var expected = 8889;
+
+        var actual = sut.getArgs([]);
+
+        assert(actual.admin === expected);
       });
+
       it('should return supplied value when provided', function () {
-        var actual, expected;
-        expected = '81';
-        actual = sut.getArgs(['-a', expected]);
-        return assert(actual.admin === expected);
+        var expected = '81';
+
+        var actual = sut.getArgs(['-a', expected]);
+
+        assert(actual.admin === expected);
       });
-      return it('should return supplied value when provided with full flag', function () {
-        var actual, expected;
-        expected = '81';
-        actual = sut.getArgs(['--admin', expected]);
-        return assert(actual.admin === expected);
+
+      it('should return supplied value when provided with full flag', function () {
+        var expected = '81';
+        var actual = sut.getArgs(['--admin', expected]);
+        assert(actual.admin === expected);
       });
     });
+
     describe('-s, --stubs', function () {
       it('should return default if no flag provided', function () {
-        var actual, expected;
-        expected = 8882;
-        actual = sut.getArgs([]);
-        return assert(actual.stubs === expected);
+        var expected = 8882;
+
+        var actual = sut.getArgs([]);
+
+        assert(actual.stubs === expected);
       });
+
       it('should return supplied value when provided', function () {
-        var actual, expected;
-        expected = '80';
-        actual = sut.getArgs(['-s', expected]);
-        return assert(actual.stubs === expected);
+        var expected = '80';
+
+        var actual = sut.getArgs(['-s', expected]);
+
+        assert(actual.stubs === expected);
       });
-      return it('should return supplied value when provided with full flag', function () {
-        var actual, expected;
-        expected = '80';
-        actual = sut.getArgs(['--stubs', expected]);
-        return assert(actual.stubs === expected);
+
+      it('should return supplied value when provided with full flag', function () {
+        var expected = '80';
+        var actual = sut.getArgs(['--stubs', expected]);
+        assert(actual.stubs === expected);
       });
     });
+
     describe('-t, --tls', function () {
       it('should return default if no flag provided', function () {
-        var actual, expected;
-        expected = 7443;
-        actual = sut.getArgs([]);
-        return assert(actual.tls === expected);
+        var expected = 7443;
+
+        var actual = sut.getArgs([]);
+
+        assert(actual.tls === expected);
       });
+
       it('should return supplied value when provided', function () {
-        var actual, expected;
-        expected = '443';
-        actual = sut.getArgs(['-t', expected]);
-        return assert(actual.tls === expected);
+        var expected = '443';
+
+        var actual = sut.getArgs(['-t', expected]);
+
+        assert(actual.tls === expected);
       });
-      return it('should return supplied value when provided with full flag', function () {
-        var actual, expected;
-        expected = '443';
-        actual = sut.getArgs(['--tls', expected]);
-        return assert(actual.tls === expected);
+
+      it('should return supplied value when provided with full flag', function () {
+        var expected = '443';
+
+        var actual = sut.getArgs(['--tls', expected]);
+
+        assert(actual.tls === expected);
       });
     });
+
     describe('-l, --location', function () {
       it('should return default if no flag provided', function () {
-        var actual, expected;
-        expected = '0.0.0.0';
-        actual = sut.getArgs([]);
-        return assert(actual.location === expected);
+        var expected = '0.0.0.0';
+
+        var actual = sut.getArgs([]);
+
+        assert(actual.location === expected);
       });
+
       it('should return supplied value when provided', function () {
-        var actual, expected;
-        expected = 'stubby.com';
-        actual = sut.getArgs(['-l', expected]);
-        return assert(actual.location === expected);
+        var expected = 'stubby.com';
+
+        var actual = sut.getArgs(['-l', expected]);
+
+        assert(actual.location === expected);
       });
-      return it('should return supplied value when provided with full flag', function () {
-        var actual, expected;
-        expected = 'stubby.com';
-        actual = sut.getArgs(['--location', expected]);
-        return assert(actual.location === expected);
+
+      it('should return supplied value when provided with full flag', function () {
+        var expected = 'stubby.com';
+
+        var actual = sut.getArgs(['--location', expected]);
+
+        assert(actual.location === expected);
       });
     });
+
     describe('-v, --version', function () {
       it('should exit the process', function () {
         sut.getArgs(['--version']);
-        return assert(process.exit.calledOnce);
+
+        assert(process.exit.calledOnce);
       });
-      return it('should print out version info', function () {
-        var version;
-        version = require('../package.json').version;
+      it('should print out version info', function () {
+        var version = require('../package.json').version;
+
         sut.getArgs(['-v']);
-        return assert(out.log.calledWith(version));
+
+        assert(out.log.calledWith(version));
       });
     });
-    return describe('-h, --help', function () {
+
+    describe('-h, --help', function () {
       it('should exit the process', function () {
         sut.getArgs(['--help']);
-        return assert(process.exit.calledOnce);
+
+        assert(process.exit.calledOnce);
       });
-      return it('should print out help text', function () {
+
+      it('should print out help text', function () {
         sut.help();
         sut.getArgs(['-h']);
 
@@ -136,9 +161,9 @@ describe('CLI', function () {
       });
     });
   });
+
   describe('data', function () {
-    var expected;
-    expected = [{
+    var expected = [{
       request: {
         url: '/testput',
         method: 'PUT',
@@ -166,47 +191,53 @@ describe('CLI', function () {
         body: null
       }
     }];
+
     it('should be about to parse json file with array', function () {
-      var actual;
-      actual = sut.getArgs(['-d', 'test/data/cli.getData.json']);
-      return assert.deepEqual(actual.data, expected);
+      var actual = sut.getArgs(['-d', 'test/data/cli.getData.json']);
+      assert.deepEqual(actual.data, expected);
     });
-    return it('should be about to parse yaml file with array', function () {
-      var actual;
-      actual = sut.getArgs(['-d', 'test/data/cli.getData.yaml']);
-      return assert.deepEqual(actual.data, expected);
+
+    it('should be about to parse yaml file with array', function () {
+      var actual = sut.getArgs(['-d', 'test/data/cli.getData.yaml']);
+      assert.deepEqual(actual.data, expected);
     });
   });
+
   describe('key', function () {
-    return it('should return contents of file', function () {
-      var actual, expected;
-      expected = 'some generated key';
-      actual = sut.key('test/data/cli.getKey.pem');
-      return assert(actual === expected);
+    it('should return contents of file', function () {
+      var expected = 'some generated key';
+
+      var actual = sut.key('test/data/cli.getKey.pem');
+
+      assert(actual === expected);
     });
   });
+
   describe('cert', function () {
-    var expected;
-    expected = 'some generated certificate';
-    return it('should return contents of file', function () {
-      var actual;
-      actual = sut.cert('test/data/cli.getCert.pem');
-      return assert(actual === expected);
+    var expected = 'some generated certificate';
+
+    it('should return contents of file', function () {
+      var actual = sut.cert('test/data/cli.getCert.pem');
+
+      assert(actual === expected);
     });
   });
+
   describe('pfx', function () {
-    return it('should return contents of file', function () {
-      var actual, expected;
-      expected = 'some generated pfx';
-      actual = sut.pfx('test/data/cli.getPfx.pfx');
-      return assert(actual === expected);
+    it('should return contents of file', function () {
+      var expected = 'some generated pfx';
+
+      var actual = sut.pfx('test/data/cli.getPfx.pfx');
+
+      assert(actual === expected);
     });
   });
-  return describe('getArgs', function () {
-    return it('should gather all arguments', function () {
-      var actual, expected, filename;
-      filename = 'file.txt';
-      expected = {
+
+  describe('getArgs', function () {
+    it('should gather all arguments', function () {
+      var actual;
+      var filename = 'file.txt';
+      var expected = {
         data: 'a file',
         stubs: '88',
         admin: '90',
@@ -225,12 +256,10 @@ describe('CLI', function () {
       this.sandbox.stub(sut, 'key').returns(expected.key);
       this.sandbox.stub(sut, 'cert').returns(expected.cert);
       this.sandbox.stub(sut, 'pfx').returns(expected.pfx);
+
       actual = sut.getArgs(['-s', expected.stubs, '-a', expected.admin, '-d', filename, '-l', expected.location, '-k', 'mocked', '-c', 'mocked', '-p', 'mocked', '-t', expected.tls, '-m', '-w']);
+
       assert.deepEqual(actual, expected);
-      sut.data.restore();
-      sut.key.restore();
-      sut.cert.restore();
-      return sut.pfx.restore();
     });
   });
 });
