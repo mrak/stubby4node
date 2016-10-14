4251c12df5f2051c0ac14bc2d1449c20b4fca4f0
Eric Mrak <mail@ericmrak.info>
2016-10-14 13:50:13 -0700

    polyfill deepStrictEqual for older node versions

--- test/global.js
+++ test/global.js
@@ -1,6 +1,11 @@
 'use strict';
 
 var sinon = require('sinon');
+var assert = require('assert');
+
+if (!assert.deepStrictEqual) {
+  assert.deepStrictEqual = assert.deepEqual;
+}
 
 beforeEach(function () {
   this.sandbox = sinon.sandbox.create();
